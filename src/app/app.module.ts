1b8e9753db23bae5da7fb5785efd64eca1d960a6
JOON HYUK LEE <anshqhsh@naver.com>
2023-04-18 11:44:32 +0900

    :truck: app component 분리

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms'; // FormsModule import 추가
 import { AppComponent } from './app.component';
+
+import { RouterModule, Routes } from '@angular/router'; // 추가
+import { TodoComponent } from './todo.component';
+import { TodoListComponent } from './todo/todo-list.component';
+import { TodoInputComponent } from './todo/todo-input.component';
+import { TodoService } from './todo/todo.service';
+import { TodoModule } from './todo.module';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home'
+  },
+  {
+    path: 'home',
+    component: AppComponent
+  },
+  {
+    path: 'todo',
+    component: TodoComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    
+    TodoModule,
+    RouterModule.forRoot(routes) // 추가
   ],
   providers: [],
   bootstrap: [AppComponent]
